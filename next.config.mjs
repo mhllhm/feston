f8fe7f4efed78ed6bee34e7201ca9207fda1c462
Michele Sartore <michele.sartore@srtcloud.com>
2024-01-28 12:53:20 +0100

    fix: added basePath property

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/github-pages',
+  
+  }
+  
+  module.exports = nextConfig
