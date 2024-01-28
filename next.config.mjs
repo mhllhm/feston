8cc2350844c5cfde8c40a7556e8329b134a6ac21
Michele Sartore <michele.sartore@srtcloud.com>
2024-01-28 12:53:47 +0100

    revert: added basePath property

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-  
-   // Add basePath
-    basePath: '/github-pages',
-  
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
+
+export default nextConfig;
